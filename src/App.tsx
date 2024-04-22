a2eba38bf1d3e39b81ca0bb69bfe8aa6d0623047
“evan-weissrussland” <evanwodichka@gmail.com>
2024-04-22 22:22:49 +0300

    e

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,88 @@
 import {IconsTelegram, IconsTwitter} from './Icons.tsx'
 import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "./store.ts";
-import {InitialStateType, setQuote} from "./slice.ts";
+import {InitialStateType, setColor, setQuote} from "./slice.ts";
 import {useSpring, animated} from '@react-spring/web'
+import {useEffect, useRef, useState} from "react";
 
 export const App = () => {
-
     const dispatch = useDispatch()
-    const state = useSelector<AppRootStateType, InitialStateType>(state => state.quote)
-    const quote = state.items.find(i => i.id === state.currentQuote)
 
-    const [props, api] = useSpring(() => ({
+
+    const items = useSelector<AppRootStateType, {
+        id: number,
+        text: string,
+        author: string,
+        color: string
+    }[]>(state => state.quote.items)
+
+    const currentQuoteId = useSelector<AppRootStateType, number>(state => state.quote.currentQuote)
+
+    const quote = items.find(i => i.id === currentQuoteId)
+
+    const stateColor = useSelector<AppRootStateType, { id: number, color: string }[]>(state => state.quote.colors)
+
+    const currentColorId = useSelector<AppRootStateType, number>(state => state.quote.currentColor)
+
+    const colorItem = stateColor.find(i => i.id === currentColorId)
+
+    const refT = useRef({col:'black', dur:500})
+
+    const [propsText, apiText] = useSpring(() => ({
         from: {opacity: 0},
         to: {opacity: 1},
-        config: {duration: 1000}
-    }),[quote]);
+        config: {duration: 500}
+    }), [currentQuoteId]);
+
+    const [propsBackgroundColor, apiMain] = useSpring(() => ({
+        from: {backgroundColor: refT.current.col},
+        to: {backgroundColor: colorItem!.color},
+        config: {duration: refT.current.dur}
+    }), [currentColorId]);
 
     const changeQuoteHandler = () => {
-        api.start({
-            from: {opacity: 1},
-            to: {opacity: 0},
+        apiText.start({
+                from: {opacity: 1},
+                to: {opacity: 0},
             }
         )
-        const id = state.items.length - state.currentQuote !== 0 ? state.currentQuote + 1 : 1
-        setTimeout(()=>{dispatch(setQuote({idQuote: id}))},1000)
+        const id = items.length - currentQuoteId !== 0 ? currentQuoteId + 1 : 1
+        const idColor = stateColor.length - currentColorId !== 0 ? currentColorId + 1 : 1
+        setTimeout(() => {
+            dispatch(setQuote({idQuote: id}))
+        }, 500)
+        dispatch(setColor({idColor: idColor}))
     }
 
+    useEffect(() => {
+        refT.current.col = colorItem!.color
+    }, [currentColorId])
+
+    useEffect(() => {
+        refT.current.dur = 1000
+    }, [])
+
     return (
-        <main style={{backgroundColor: `${quote!.color}`, color: `${quote!.color}`}}>
+        <animated.main style={{...propsBackgroundColor, color: `${quote!.color}`}}>
             <section>
-                <animated.div style={props}>
-                    <h1>{quote!.text}</h1>
-                    <p>-{quote!.author}</p>
+                <animated.div style={propsText}>
+                    <h1>{ quote!.text }</h1>
+                    <p>-{ quote!.author }</p>
                 </animated.div>
 
                 <footer>
                     <div className={'icons'}>
-                        <a title={'aaaaaaaaaaaa'} style={{backgroundColor: `${quote!.color}`}} href="#">
+                        <animated.a title={'aaaaaaaaaaaa'} style={propsBackgroundColor} href="#">
                             <IconsTwitter/>
-                        </a>
-                        <a title={'ssssssssss'} style={{backgroundColor: `${quote!.color}`}} href="#">
+                        </animated.a>
+                        <animated.a title={'ssssssssss'} style={propsBackgroundColor} href="#">
                             <IconsTelegram/>
-                        </a>
+                        </animated.a>
                     </div>
-                    <button style={{backgroundColor: `${quote!.color}`}} onClick={changeQuoteHandler}>New quote</button>
+                    <animated.button style={propsBackgroundColor} onClick={changeQuoteHandler}>New quote
+                    </animated.button>
                 </footer>
             </section>
-        </main>
+        </animated.main>
     )
 }
--- src/slice.ts
+++ src/slice.ts
@@ -1,27 +1,38 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit/react"
+import {createSlice, PayloadAction} from "@reduxjs/toolkit/react"
 
 const initialState = {
     items: [
-        {id:1, text:'one text', author:'me author', color:'green'},
-        {id:2, text:'two text', author:'dick', color:'yellowgreen'},
-        {id:3, text:'tree text', author:'card', color:'orange'},
-        {id:4, text:'four text', author:'bilbo', color:'gray'},
-        {id:5, text:'five text', author:'gicci', color:'red'},
+        {id: 1, text: 'one text', author: 'me author', color: 'green'},
+        {id: 2, text: 'two text', author: 'dick', color: 'yellowgreen'},
+        {id: 3, text: 'tree text', author: 'card', color: 'orange'},
+        {id: 4, text: 'four text', author: 'bilbo', color: 'gray'},
+        {id: 5, text: 'five text', author: 'gicci', color: 'red'},
     ],
-    currentQuote:1
+    currentQuote: 1,
+    colors: [
+        {id: 1, color: 'green'},
+        {id: 2, color: 'yellowgreen'},
+        {id: 3, color: 'orange'},
+        {id: 4, color: 'gray'},
+        {id: 5, color: 'red'}
+    ],
+    currentColor: 1,
 }
 
 export type InitialStateType = typeof initialState
 
 export const slice = createSlice({
-    name:'quote',
+    name: 'quote',
     initialState: initialState,
     reducers: {
-        setQuote(state, action:PayloadAction<{idQuote:number}>) {
+        setQuote(state, action: PayloadAction<{ idQuote: number }>) {
             state.currentQuote = action.payload.idQuote
+        },
+        setColor(state, action: PayloadAction<{ idColor: number }>) {
+            state.currentColor = action.payload.idColor
         }
     },
 })
 
 export const qouteReducer = slice.reducer
-export const {setQuote} = slice.actions
+export const {setQuote,setColor} = slice.actions
