e4d3e19cce918a050a6513f85c7c64994db671a4
“evan-weissrussland” <evanwodichka@gmail.com>
2024-05-10 22:37:52 +0300

    add deploy to github

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
+export default defineConfig( {
     plugins: [react()],
-    base: '/',
-  }
-
-  if (command !== 'serve') {
-    config.base = '/Random-Quote-Machine/'
-  }
-
-  return config
+    base: '/Random-Quote-Machine/'
 })
